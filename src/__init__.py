90ff534a8de31737c0ffaed7ed3ee69cc19d45e3
Vinicios Gabriel <109308457+Vinicios-Developer@users.noreply.github.com>
2025-01-06 00:30:05 -0400

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,3 @@
-
-from src import routes
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
@@ -21,3 +19,5 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'alert-info'
+
+from src import routes
