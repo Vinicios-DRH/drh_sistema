0027b6cc72ccd259aa5f82435794b29e7ce4d5ca
Vinicios-Developer <viniciosgabriel_18@hotmail.com>
2025-01-06 00:11:04 -0400

    erro no supabase

--- src/__init__.py
+++ src/__init__.py
@@ -1,3 +1,4 @@
+from src import routes
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
@@ -19,5 +20,3 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'alert-info'
-
-from src import routes
